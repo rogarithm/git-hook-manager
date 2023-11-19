c7cb303afecd9c5200090e2ec51a4d47996dae5c
sehoon.gim <sehoongim@gmail.com>
2023-11-19 20:11:40 +0900

    프로젝트 분리 후 깨지는 테스트를 고친다

--- installer/spec/install_spec.rb
+++ installer/spec/install_spec.rb
@@ -12,14 +12,14 @@ describe "Installer", "operations" do
   after(:each) do
     @installer.uninstall_hook(
       @installer.make_install_infos(
-        @installer.prepare_paths_to_install('./spec/data/install_list')
+        @installer.prepare_paths_to_install('./installer/spec/data/install_list')
       )
     )
   end
 
   it "설정 파일 내용으로부터 훅을 설치하는 데 필요한 정보를 계산할 수 있다" do
     @installer.make_install_infos(
-      @installer.prepare_paths_to_install('./spec/data/install_list2')
+      @installer.prepare_paths_to_install('./installer/spec/data/install_list2')
     ).should == [[File.expand_path(@install_full_dir), @trigger_point]]
   end
   it "훅 발동 조건으로 훅 소스 위치를 계산할 수 있다" do
@@ -30,7 +30,7 @@ describe "Installer", "operations" do
   it "훅을 설치할 위치와 발동 조건을 입력하면 훅을 설치한다" do
     @installer.install_hook(
       @installer.make_install_infos(
-        @installer.prepare_paths_to_install('./spec/data/install_list2')
+        @installer.prepare_paths_to_install('./installer/spec/data/install_list2')
       )
     )
     File.symlink?(@install_full_dir).should == true
@@ -38,7 +38,7 @@ describe "Installer", "operations" do
 
   it "설정 파일 내용으로부터 여러 개의 훅을 설치하는 데 필요한 정보를 계산할 수 있다" do
     @installer.make_install_infos(
-      @installer.prepare_paths_to_install('./spec/data/install_list')
+      @installer.prepare_paths_to_install('./installer/spec/data/install_list')
     ).should ==
     [
       [File.expand_path(@install_full_dir), @trigger_point],
@@ -48,7 +48,7 @@ describe "Installer", "operations" do
   it "한 번에 두 곳에 훅을 설치할 수 있다" do
     @installer.install_hook(
       @installer.make_install_infos(
-        @installer.prepare_paths_to_install('./spec/data/install_list')
+        @installer.prepare_paths_to_install('./installer/spec/data/install_list')
       )
     )
     File.symlink?(@install_full_dir).should == true
@@ -56,10 +56,10 @@ describe "Installer", "operations" do
   end
 
   it "훅을 설치할 경로와 발동 조건 정보를 파일에서 읽어올 수 있다" do
-    @installer.prepare_paths_to_install('./spec/data/install_list').should == [['/tmp/test_install_git_hook', @trigger_point], ['/tmp/test_install_git_hook2', @trigger_point]]
+    @installer.prepare_paths_to_install('./installer/spec/data/install_list').should == [['/tmp/test_install_git_hook', @trigger_point], ['/tmp/test_install_git_hook2', @trigger_point]]
   end
 
   it "훅이 설치되었는지 확인할 수 있다" do
-    @installer.check_hooks_installed('./spec/data/install_list')
+    @installer.check_hooks_installed('./installer/spec/data/install_list')
   end
 end
