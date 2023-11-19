81413b5460d5340296e57fe0f79d0594c4dcbec7
sehoon.gim <sehoongim@gmail.com>
2023-11-19 22:27:14 +0900

    테스트에서 출력문을 지운다

--- installer/spec/ruby_spec.rb
+++ installer/spec/ruby_spec.rb
@@ -1,39 +1,47 @@
 describe "Regex", "operations" do
   it "콤마 기준으로 앞쪽 문자열만 가져올 수 있다" do
+    result = []
     str = "/tmp/test_install_git_hook, pre-push"
     str.split(',').each_with_index do |item, index|
-      puts "#{index}: #{item}"
+      result << "#{item.strip}"
     end
+
+    result.should == ['/tmp/test_install_git_hook', 'pre-push']
   end
 
   it "문자열 앞뒤의 공백을 지울 수 있다" do
+    result = []
     str = "/tmp/test_install_git_hook, pre-push"
     str.split(',').each_with_index do |item, index|
-      puts "#{index}: >#{item.strip}<"
+      result << "#{item.strip}"
     end
+
+    result.should == ['/tmp/test_install_git_hook', 'pre-push']
   end
 
   it "" do
+    result = []
     File.open('installer/spec/data/install_list', "r") do |f|
       f.each_line do |line|
-        puts ">#{line.split(',')[0].strip}<"
+        result << "#{line.split(',')[0].strip}"
       end
     end
+    result.should == ['/tmp/test_install_git_hook', '/tmp/test_install_git_hook2']
   end
 
   it "push array to array" do
+    result = []
     install_info = []
     File.open('installer/spec/data/install_list', "r") do |f|
       f.each_line do |line|
-        install_info.push([
+        result.push([
             line.split(',')[0].strip,
             line.split(',')[1].strip
         ])
       end
     end
-    install_info.each_with_index do |info, index|
-      puts "#{index}: #{info.inspect}"
-    end
+
+    result.should == [["/tmp/test_install_git_hook", "pre-push"], ["/tmp/test_install_git_hook2", "pre-push"]]
   end
 
   it "" do
@@ -41,6 +49,6 @@ describe "Regex", "operations" do
     [['a', 'b'], ['c', 'd']].each do |item|
       result.push(item[0])
     end
-    puts "#{result.inspect}"
+    result.should == ['a', 'c']
   end
 end
