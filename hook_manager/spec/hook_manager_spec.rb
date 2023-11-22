7e0d8377232ac2226a9c2cae424a5ad2a2bde962
sehoon.gim <sehoongim@gmail.com>
2023-11-22 13:46:11 +0900

    테스트 순서를 바꾼다

--- hook_manager/spec/hook_manager_spec.rb
+++ hook_manager/spec/hook_manager_spec.rb
@@ -19,30 +19,20 @@ describe "Hook Manager", "operations" do
     )
   end
 
+
   #it "설정 정보를 갖는 파일을 로드할 수 있다" do
   #  @hook_manager.load_config
   #end
+  it "훅 설정 정보를 가져올 수 있다" do
+    @hook_manager.prepare_paths_to_install('./hook_manager/spec/data/install_list').should == [['/tmp/test_install_git_hook', @trigger_point, @hook_name], ['/tmp/test_install_git_hook2', @trigger_point, @hook_name]]
+  end
+
 
   it "설정 파일 내용으로부터 훅을 설치하는 데 필요한 정보를 계산할 수 있다" do
     @hook_manager.make_install_infos(
       @hook_manager.prepare_paths_to_install('./hook_manager/spec/data/install_list2')
     ).should == [[File.expand_path(@install_full_dir), @trigger_point, @hook_name]]
   end
-  it "동일한 발동 조건에 여러 훅이 있을 경우 훅 소스 위치를 계산할 때 훅 이름을 지정할 수 있다" do
-    @hook_manager.find_hook_location(
-      @trigger_point,
-      'is_remote_updated'
-    ).should == File.expand_path('~/tools/git-hooks/pre_push/bin/is_remote_updated')
-  end
-  it "훅을 설치할 위치와 발동 조건을 입력하면 훅을 설치한다" do
-    @hook_manager.install_hook(
-      @hook_manager.make_install_infos(
-        @hook_manager.prepare_paths_to_install('./hook_manager/spec/data/install_list2')
-      )
-    )
-    File.symlink?(@install_full_dir).should == true
-  end
-
   it "설정 파일 내용으로부터 여러 개의 훅을 설치하는 데 필요한 정보를 계산할 수 있다" do
     @hook_manager.make_install_infos(
       @hook_manager.prepare_paths_to_install('./hook_manager/spec/data/install_list')
@@ -51,6 +41,24 @@ describe "Hook Manager", "operations" do
       [File.expand_path(@install_full_dir), @trigger_point, @hook_name],
       [File.expand_path(@install_full_dir2), @trigger_point, @hook_name]
     ]
+  end
+
+
+  it "동일한 발동 조건에 여러 훅이 있을 경우 훅 소스 위치를 계산할 때 훅 이름을 지정할 수 있다" do
+    @hook_manager.find_hook_location(
+      @trigger_point,
+      'is_remote_updated'
+    ).should == File.expand_path('~/tools/git-hooks/pre_push/bin/is_remote_updated')
+  end
+
+
+  it "훅을 설치할 위치와 발동 조건을 입력하면 훅을 설치한다" do
+    @hook_manager.install_hook(
+      @hook_manager.make_install_infos(
+        @hook_manager.prepare_paths_to_install('./hook_manager/spec/data/install_list2')
+      )
+    )
+    File.symlink?(@install_full_dir).should == true
   end
   it "한 번에 두 곳에 훅을 설치할 수 있다" do
     @hook_manager.install_hook(
@@ -62,9 +70,6 @@ describe "Hook Manager", "operations" do
     File.symlink?(@install_full_dir2).should == true
   end
 
-  it "훅을 설치할 경로와 발동 조건 정보를 파일에서 읽어올 수 있다" do
-    @hook_manager.prepare_paths_to_install('./hook_manager/spec/data/install_list').should == [['/tmp/test_install_git_hook', @trigger_point, @hook_name], ['/tmp/test_install_git_hook2', @trigger_point, @hook_name]]
-  end
 
   it "훅이 설치되었는지 확인할 수 있다" do
     @hook_manager.check_hooks_installed('./hook_manager/spec/data/install_list')
